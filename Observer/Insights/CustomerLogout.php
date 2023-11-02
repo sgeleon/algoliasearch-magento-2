9ce9ec437a296527ba2ecd25ffb7532422d14bd1
Mohit Choudhary <mohit.choudhary@algolia.com>
2023-11-02 17:25:14 +0530

    MAGE-787 Fixed issue with cookies not cleared when customer logs out

---
+++ Observer/Insights/CustomerLogout.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Observer\Insights;
+
+use Algolia\AlgoliaSearch\Helper\InsightsHelper;
+use Magento\Customer\Model\Customer;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class CustomerLogout implements ObserverInterface
+{
+
+    /** @var InsightsHelper */
+    protected $insightsHelper;
+
+    /**
+     * @param InsightsHelper $insightsHelper
+     */
+    public function __construct(
+        InsightsHelper $insightsHelper
+    )
+    {
+        $this->insightsHelper = $insightsHelper;
+    }
+
+    /**
+     * @param Observer $observer
+     * ['customer' => $customer]
+     */
+    public function execute(Observer $observer)
+    {
+        /** @var Customer $customer */
+        $customer = $observer->getEvent()->getCustomer();
+
+        if ($this->insightsHelper->getConfigHelper()->isClickConversionAnalyticsEnabled($customer->getStoreId()) || $this->insightsHelper->getPersonalizationHelper()->isPersoEnabled($customer->getStoreId())) {
+            $this->insightsHelper->unsetUserToken($customer);
+        }
+    }
+}
