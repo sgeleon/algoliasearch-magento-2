dd8ca941d072f82cff432aaa6118a979f8bc31f2
Mohit Choudhary <mohit.choudhary@algolia.com>
2023-11-02 18:48:29 +0530

    MAGE-787 Fixed issue with cookies not cleared when customer logs out

--- Helper/InsightsHelper.php
+++ Helper/InsightsHelper.php
@@ -171,17 +171,4 @@ class InsightsHelper
         return $userToken;
     }
 
-    /**
-     * @param Customer $customer
-     * @return void
-     */
-    public function unsetUserToken(Customer $customer)
-    {
-        if ($this->cookieManager->getCookie(self::ALGOLIA_CUSTOMER_USER_TOKEN_COOKIE_NAME)) {
-            $metadata = $this->cookieMetadataFactory->createCookieMetadata()
-                ->setPath($this->sessionManager->getCookiePath());
-            return $this->cookieManager->deleteCookie(
-                self::ALGOLIA_CUSTOMER_USER_TOKEN_COOKIE_NAME,$metadata);
-        }
-    }
 }
