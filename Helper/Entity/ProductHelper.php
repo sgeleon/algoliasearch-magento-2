bc8ee3d27e850a325e65924c8ecee2be6aa7953d
mohitalgolia <101385480+mohitalgolia@users.noreply.github.com>
2023-11-09 16:17:05 +0530

    Duplicate Replica Issue one set is standard and another is virtual

--- Helper/Entity/ProductHelper.php
+++ Helper/Entity/ProductHelper.php
@@ -1558,12 +1558,14 @@ class ProductHelper
                 return $sortingIndex['name'];
             }, $sortingIndices));
             try {
+                $replicasFormated = $this->handleVirtualReplica($replicas);
+                $availableReplicaMatch = array_merge($replicasFormated, $replicas);
                 if ($this->configHelper->useVirtualReplica($storeId)) {
-                    $replicas = $this->handleVirtualReplica($replicas, $indexName);
+                   $replicas = $replicasFormated;
                 }
                 $currentSettings = $this->algoliaHelper->getSettings($indexName);
                 if (is_array($currentSettings) && array_key_exists('replicas', $currentSettings)) {
-                    $replicasRequired = array_values(array_diff_assoc($currentSettings['replicas'], $replicas));
+                    $replicasRequired = array_values(array_diff($currentSettings['replicas'], $availableReplicaMatch));
                     $this->algoliaHelper->setSettings($indexName, ['replicas' => $replicasRequired]);
                     $setReplicasTaskId = $this->algoliaHelper->getLastTaskId();
                     $this->algoliaHelper->waitLastTask($indexName, $setReplicasTaskId);
