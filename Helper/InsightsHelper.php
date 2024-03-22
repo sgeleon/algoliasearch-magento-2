bd24cfbc34e4875bbabe23f34088f724f0670efa
Eric Wright <eric.wright@algolia.com>
2024-03-22 18:24:10 -0400

    MAGE-780 Refactor for more unique pseudo anonymous token with PII obfuscated

--- Helper/InsightsHelper.php
+++ Helper/InsightsHelper.php
@@ -5,10 +5,10 @@ namespace Algolia\AlgoliaSearch\Helper;
 use Algolia\AlgoliaSearch\Api\Insights\EventsInterface;
 use Algolia\AlgoliaSearch\Api\Insights\EventsInterfaceFactory;
 use Algolia\AlgoliaSearch\Api\InsightsClient;
-use Algolia\AlgoliaSearch\Exceptions\AlgoliaException;
 use Algolia\AlgoliaSearch\Helper\Configuration\PersonalizationHelper;
 use Magento\Customer\Model\Customer;
 use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
 use Magento\Framework\Stdlib\CookieManagerInterface;
 use Magento\Store\Model\StoreManagerInterface;
@@ -19,6 +19,10 @@ class InsightsHelper
     public const ALGOLIA_ANON_USER_TOKEN_COOKIE_NAME = '_ALGOLIA';
     /** @var string  */
     public const ALGOLIA_CUSTOMER_USER_TOKEN_COOKIE_NAME = '_ALGOLIA_MAGENTO_AUTH';
+    /** @var string */
+    public const ALGOLIA_CUSTOMER_USER_TOKEN_PREFIX = 'aa-';
+    /** @var int */
+    public const ALGOLIA_USER_TOKEN_MAX_LENGTH = 129;
 
     /** @var string */
     public const QUOTE_ITEM_QUERY_PARAM = 'algoliasearch_query_param';
@@ -52,7 +56,8 @@ class InsightsHelper
         private readonly CookieMetadataFactory  $cookieMetadataFactory,
         private readonly CustomerSession        $customerSession,
         private readonly EventsInterfaceFactory $eventsFactory,
-        private readonly StoreManagerInterface  $storeManager
+        private readonly StoreManagerInterface  $storeManager,
+        private readonly Logger                 $logger
     ) { }
 
     public function getPersonalizationHelper(): PersonalizationHelper
@@ -99,7 +104,7 @@ class InsightsHelper
 
     public function getAnonymousUserToken(): string
     {
-        return $this->cookieManager->getCookie(self::ALGOLIA_ANON_USER_TOKEN_COOKIE_NAME);
+        return (string) $this->cookieManager->getCookie(self::ALGOLIA_ANON_USER_TOKEN_COOKIE_NAME) ?? "";
     }
 
     public function getAuthenticatedUserToken(): string
@@ -115,25 +120,42 @@ class InsightsHelper
     }
 
     /**
+     * Generate a pseudo anonymous token compliant with Algolia spec:
+     * https://www.algolia.com/doc/api-reference/api-methods/set-authenticated-user-token/#method-param-authenticatedusertoken
+     * Uniquely identify the user for this Magento store but obfuscate any PII
+     *
+     * @param Customer $customer
+     * @return string
+     */
+    public function generateAuthenticatedUserToken(Customer $customer): string
+    {
+        $hash = hash('sha256', $customer->getEmail());
+        $userToken = base64_encode('customer-' . $hash . '-' . $customer->getId());
+        $userToken = self::ALGOLIA_CUSTOMER_USER_TOKEN_PREFIX . preg_replace('/[^A-Za-z0-9_=+\/\-]/', '', $userToken);
+        $userToken = mb_substr($userToken, 0, self::ALGOLIA_USER_TOKEN_MAX_LENGTH);
+        return $userToken;
+    }
+
+    /**
      * For a Magento customer, generated an authentication token to be used for personalization and insights
+     * and store as a cookie in the browser (requires customer consent)
      * @param Customer $customer
      * @return string|null The user token that was generated for the customer, null if unable to create
      */
     public function setAuthenticatedUserToken(Customer $customer): string|null
     {
-        $userToken = base64_encode('customer-' . $customer->getId());
-        $userToken = 'aa-' . preg_replace('/[^A-Za-z0-9\-]/', '', $userToken);
-        $userToken = mb_substr($userToken, 0, 64); // character limit
+        $userToken = $this->generateAuthenticatedUserToken($customer);
+
+        $metaData = $this->cookieMetadataFactory->createPublicCookieMetadata()
+            ->setDurationOneYear()
+            ->setPath('/')
+            ->setHttpOnly(false)
+            ->setSecure(false);
 
         try {
-            $metaData = $this->cookieMetadataFactory->createPublicCookieMetadata()
-                ->setDurationOneYear()
-                ->setPath('/')
-                ->setHttpOnly(false)
-                ->setSecure(false);
             $this->cookieManager->setPublicCookie(self::ALGOLIA_CUSTOMER_USER_TOKEN_COOKIE_NAME, $userToken, $metaData);
-        } catch (\Exception $e) {
-
+        } catch (LocalizedException $e) {
+            $this->logger->error("Error writing Algolia customer cookie: " . $e->getMessage());
             $userToken = null;
         }
 
