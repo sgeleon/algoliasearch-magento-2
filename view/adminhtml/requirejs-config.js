3145fd5994beef2aafe40968172d3058bf97f98f
Eric Wright <eric.wright@algolia.com>
2024-06-05 09:28:53 -0400

    MAGE-721 Remove debugging code from admin query rule handler on facets

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,11 +1,5 @@
 var config = {
     paths: {
         algoliaAdminBundle: 'Algolia_AlgoliaSearch/algoliaAdminBundle.min',
-    },
-    // Callback when the node is created
-    // onNodeCreated: function (node, config, moduleName, url) {
-    //   console.log(
-    //     `Creating script tag for module: ${moduleName} with URL: ${url}`
-    //   );
-    // },
+    }
 };
--- view/adminhtml/web/js/config.js
+++ view/adminhtml/web/js/config.js
@@ -1,5 +1,3 @@
-const observing = [];
-
 require(
 	[
 		'jquery',
@@ -11,40 +9,40 @@ require(
 
 		function addDashboardWarnings() {
 			// rows
-			var rowIds = [
+			const rowIds = [
 				'#row_algoliasearch_instant_instant_facets_facets',
 				'#row_algoliasearch_instant_instant_facets_max_values_per_facet'
 			];
 
-			var rowWarning = '<div class="algolia_dashboard_warning">';
+			let rowWarning = '<div class="algolia_dashboard_warning">';
 			rowWarning += '<p>This setting is also available in the Algolia Dashboard. We advise you to manage it from this page, because saving Magento settings will override the Algolia settings.</p>';
 			rowWarning += '</div>';
 
-			for (var i=0; i < rowIds.length; i++) {
-				var element = $(rowIds[i]);
+			for (let i=0; i < rowIds.length; i++) {
+				const element = $(rowIds[i]);
 				if (element.length > 0) {
 					element.find('.value').prepend(rowWarning);
 				}
 			}
 
 			// pages
-			var pageIds = [
+			const pageIds = [
 				'#algoliasearch_products_products',
 				'#algoliasearch_categories_categories',
 				'#algoliasearch_credentials_credentials',
 				'#algoliasearch_extra_settings_extra_settings'
 			];
 
-			var pageWarning = '<div class="algolia_dashboard_warning algolia_dashboard_warning_page">';
+			let pageWarning = '<div class="algolia_dashboard_warning algolia_dashboard_warning_page">';
 			pageWarning += '<p>These settings are also available in the Algolia Dashboard. We advise you to manage it from this page, cause saving Magento settings will override the Algolia settings.</p>';
 			pageWarning += '</div>';
 
-			var pageWarningSynonyms = '<div class="algolia_dashboard_warning algolia_dashboard_warning_page">';
+			let pageWarningSynonyms = '<div class="algolia_dashboard_warning algolia_dashboard_warning_page">';
 			pageWarningSynonyms += '<p>Configurations related to Synonyms have been removed from the Magento dashboard. We advise you to configure synonyms from the Algolia dashboard.</p>';
 			pageWarningSynonyms += '</div>';
 
-			for (var i=0; i < pageIds.length; i++) {
-				var element = $(pageIds[i]);
+			for (let i=0; i < pageIds.length; i++) {
+				const element = $(pageIds[i]);
 				if (element.length > 0 && pageIds[i] != "#algoliasearch_credentials_credentials") {
 					element.find('.comment').append(pageWarning);
 				} else if (element.length > 0 && pageIds[i] == "#algoliasearch_credentials_credentials"){
@@ -54,7 +52,7 @@ require(
 		}
 
 		if ($('#algoliasearch_instant_instant_facets_facets').length > 0) {
-			var addButton = $('#algoliasearch_instant_instant_facets tfoot .action-add');
+			const addButton = $('#algoliasearch_instant_instant_facets tfoot .action-add');
 			addButton.on('click', function(){
 				handleFacetQueryRules();
 			});
@@ -63,63 +61,29 @@ require(
 		}
 
 		function handleFacetQueryRules() {
-			var facets = $('#algoliasearch_instant_instant_facets_facets tbody tr');
+			const facets = $('#algoliasearch_instant_instant_facets_facets tbody tr');
 
-			for (var i=0; i < facets.length; i++) {
-				let rowId = $(facets[i]).attr('id');
-				console.log("Row ID:", rowId);
-				let searchableSelect = $('select[name="groups[instant_facets][fields][facets][value][' + rowId + '][searchable]"]');
+			for (let i=0; i < facets.length; i++) {
+				const rowId = $(facets[i]).attr('id');
+				const searchableSelect = $('select[name="groups[instant_facets][fields][facets][value][' + rowId + '][searchable]"]');
 
 				searchableSelect.on('change', function(){
 					configQrFromSearchableSelect($(this));	
 				});
 
-				// setTimeout(() => { 
-					// console.log("Invoke on delay");
-					configQrFromSearchableSelect(searchableSelect) 
-				// }, 100);
-					
+				configQrFromSearchableSelect(searchableSelect);
 			}
 		}
 
 		function configQrFromSearchableSelect(searchableSelect) {
-			var rowId = searchableSelect.parent().parent().attr('id');
+			const rowId = searchableSelect.parent().parent().attr('id');
 			const qrSelectId = 'select[name="groups[instant_facets][fields][facets][value][' + rowId + '][create_rule]"]';
-
-			if (!observing[qrSelectId]) {
-
-				const targetNode = document.querySelector(qrSelectId);
-				const config = { attributes: true, childList: false, subtree: false, attributeOldValue : true };
-				// Callback function to execute when mutations are observed
-				const callback = function (mutationsList, observer) {
-					console.log("MutationObserver callback triggered.");
-					console.trace();
-					for (var mutation of mutationsList) {
-						if (mutation.type === 'attributes') {
-							console.log(
-								'The ' + mutation.attributeName + ' attribute was modified: ',
-								performance.now()
-							);
-						}
-						console.log(mutation);
-					}
-				};
-				const observer = new MutationObserver(callback);
-				observer.observe(targetNode, config);
-
-				observing[qrSelectId] = true;
-			}
-			
-			// var searchableSelect = $('select[name="groups[instant_facets][fields][facets][value][' + rowId + '][searchable]"]');
-			var qrSelect = $(qrSelectId);
+			const qrSelect = $(qrSelectId);
 			if (qrSelect.length > 0) {
 				if (searchableSelect.val() == "2") {
 					qrSelect.val('2');
-					qrSelect.attr('disabled','disabled');
 					qrSelect.attr('readonly', 'readonly');
-					console.log("Time of disabling: ", performance.now());
 				} else {
-					qrSelect.removeAttr('disabled');
 					qrSelect.removeAttr('readonly');
 				}
 			} else {
