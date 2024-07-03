0b06f755609a077d15c7882c3315a9974e256e3f
Eric Wright <eric.wright@algolia.com>
2024-07-03 13:37:48 -0400

    MAGE-938 Implement replica sync command with variable store ID argument

--- Console/Command/ReplicaCommand.php
+++ Console/Command/ReplicaCommand.php
@@ -3,7 +3,15 @@
 namespace Algolia\AlgoliaSearch\Console\Command;
 
 use Algolia\AlgoliaSearch\Api\Product\ReplicaManagerInterface;
+use Algolia\AlgoliaSearch\Exceptions\AlgoliaException;
+use Algolia\AlgoliaSearch\Exceptions\ExceededRetriesException;
+use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
+use Magento\Framework\App\Area;
+use Magento\Framework\App\State;
 use Magento\Framework\Console\Cli;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
@@ -11,15 +19,25 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ReplicaCommand extends Command
 {
-    const STORE_ARGUMENT = 'store';
+    protected const STORE_ARGUMENT = 'store';
+
+    protected ?OutputInterface $output = null;
+
+    /** @var string[] */
+    protected array $_storeNames = [];
 
     /**
+     * @param ProductHelper $productHelper
      * @param ReplicaManagerInterface $replicaManager
+     * @param StoreManagerInterface $storeManager
      * @param string|null $name
      */
     public function __construct(
+        protected State                   $state,
+        protected ProductHelper           $productHelper,
         protected ReplicaManagerInterface $replicaManager,
-        ?string $name = null
+        protected StoreManagerInterface   $storeManager,
+        ?string                           $name = null
     )
     {
         parent::__construct($name);
@@ -33,25 +51,105 @@ class ReplicaCommand extends Command
         $this->setName('algolia:replicas:sync')
             ->setDescription('Sync configured sorting attributes in Magento to Algolia replica indices')
             ->setDefinition([
-                new InputArgument(self::STORE_ARGUMENT, InputArgument::OPTIONAL, 'ID for store to be synced with Algolia (optional), if not specified all stores will be synced'),
+                new InputArgument(
+                    self::STORE_ARGUMENT,
+                    InputArgument::OPTIONAL | InputArgument::IS_ARRAY,
+                    'ID(s) for store to be synced with Algolia (optional), if not specified all stores will be synced'
+                )
             ]);
 
         parent::configure();
     }
 
-    /** @inheritDoc */
+    /**
+     * @inheritDoc
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     * @throws AlgoliaException
+     * @throws ExceededRetriesException
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $storeIds = $input->getArgument(self::STORE_ARGUMENT);
+        $storeIds = (array) $input->getArgument(self::STORE_ARGUMENT);
 
+        $msg = 'Syncing replicas for ' . ($storeIds ? count($storeIds) : 'all') . ' store' . (!$storeIds || count($storeIds) > 1 ? 's' : '');
         if ($storeIds) {
-            $output->writeln('<info>Syncing store ' . $storeIds . '!</info>');
+            /** @var string[] $storeNames */
+            $storeNames = array_map(
+                function($storeId) {
+                    return $this->getStoreName($storeId);
+                },
+                $storeIds
+            );
+            $output->writeln("<info>$msg: " . join(", ", $storeNames) . '</info>');
         } else {
-            $output->writeln('<info>Syncing all stores</info>');
+            $output->writeln("<info>$msg</info>");
         }
 
-//        $this->replicaManager->syncReplicasToAlgolia();
+        $this->output = $output;
+        $this->state->setAreaCode(Area::AREA_ADMINHTML);
+        $this->syncReplicas($storeIds);
 
         return Cli::RETURN_SUCCESS;
+    }
+
+    /**
+     * @throws NoSuchEntityException
+     */
+    protected function getStoreName(int $storeId): string
+    {
+        if (!isset($this->_storeNames[$storeId])) {
+            $this->_storeNames[$storeId] = $this->storeManager->getStore($storeId)->getName();
+        }
+        return $this->_storeNames[$storeId];
+    }
+
+    /**
+     * @param int[] $storeIds
+     * @return void
+     * @throws AlgoliaException
+     * @throws ExceededRetriesException
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    protected function syncReplicas(array $storeIds = []): void
+    {
+        if (count($storeIds)) {
+            foreach ($storeIds as $storeId) {
+                $this->syncReplicasForStore($storeId);
+            }
+        } else {
+          $this->syncReplicasForAllStores();
+        }
+    }
+
+    /**
+     * @throws NoSuchEntityException
+     * @throws ExceededRetriesException
+     * @throws AlgoliaException
+     * @throws LocalizedException
+     */
+    protected function syncReplicasForStore(int $storeId): void
+    {
+        $this->output->writeln('<info>Syncing ' . $this->getStoreName($storeId) . '...</info>');
+        $this->replicaManager->syncReplicasToAlgolia($storeId, $this->productHelper->getIndexSettings($storeId));
+    }
+
+    /**
+     * @throws NoSuchEntityException
+     * @throws ExceededRetriesException
+     * @throws AlgoliaException
+     * @throws LocalizedException
+     */
+    protected function syncReplicasForAllStores(): void
+    {
+        $storeIds = array_keys($this->storeManager->getStores());
+        foreach ($storeIds as $storeId) {
+            $this->syncReplicasForStore($storeId);
+        }
+
     }
 }
