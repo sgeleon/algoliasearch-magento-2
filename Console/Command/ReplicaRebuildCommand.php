68e32284a409482456d66998e8b7511de24147ae
Eric Wright <eric.wright@algolia.com>
2024-07-04 13:41:22 -0400

    MAGE-938 Implement replica rebuild operations via CLI

--- Console/Command/ReplicaRebuildCommand.php
+++ Console/Command/ReplicaRebuildCommand.php
@@ -2,25 +2,88 @@
 
 namespace Algolia\AlgoliaSearch\Console\Command;
 
-class ReplicaRebuildCommand extends AbstractReplicaCommand
+use Algolia\AlgoliaSearch\Api\Console\ReplicaDeleteCommandInterface;
+use Algolia\AlgoliaSearch\Api\Console\ReplicaSyncCommandInterface;
+use Algolia\AlgoliaSearch\Api\Product\ReplicaManagerInterface;
+use Algolia\AlgoliaSearch\Console\Traits\ReplicaDeleteCommandTrait;
+use Algolia\AlgoliaSearch\Console\Traits\ReplicaSyncCommandTrait;
+use Algolia\AlgoliaSearch\Exception\ReplicaLimitExceededException;
+use Algolia\AlgoliaSearch\Exceptions\BadRequestException;
+use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
+use Algolia\AlgoliaSearch\Service\StoreNameFetcher;
+use Magento\Framework\App\State;
+use Magento\Framework\Console\Cli;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ReplicaRebuildCommand
+    extends AbstractReplicaCommand
+    implements ReplicaSyncCommandInterface, ReplicaDeleteCommandInterface
 {
+    use ReplicaSyncCommandTrait;
+    use ReplicaDeleteCommandTrait;
+
+    public function __construct(
+        protected ReplicaManagerInterface $replicaManager,
+        protected StoreNameFetcher        $storeNameFetcher,
+        protected ProductHelper           $productHelper,
+        State                             $state,
+        ?string                           $name = null
+    )
+    {
+        parent::__construct($state, $name);
+    }
+
     protected function getReplicaCommandName(): string
     {
         return 'rebuild';
     }
 
-    protected function getCommandDescription(): string {
+    protected function getCommandDescription(): string
+    {
         return 'Rebuild replica configuration for Magento sorting attributes';
     }
 
     protected function getStoreArgumentDescription(): string
     {
-       return 'ID(s) for store(s) to rebuild replicas';
+        return 'ID(s) for store(s) to rebuild replicas (optional), if not specified all store replicas will be rebuilt';
     }
 
     protected function getAdditionalDefinition(): array
     {
-       return [];
+        return [];
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->output = $output;
+        $this->setAreaCode();
+
+        $storeIds = $this->getStoreIds($input);
+
+        $msg = 'Rebuilding replicas for ' . ($storeIds ? count($storeIds) : 'all') . ' store' . (!$storeIds || count($storeIds) > 1 ? 's' : '');
+        if ($storeIds) {
+            $output->writeln("<info>$msg: " . join(", ", $this->storeNameFetcher->getStoreNames($storeIds)) . '</info>');
+        } else {
+            $output->writeln("<info>$msg</info>");
+        }
+
+        $this->deleteReplicas($storeIds);
+        try {
+            $this->syncReplicas($storeIds);
+        } catch (ReplicaLimitExceededException $e) {
+            $this->output->writeln('<error>' . $e->getMessage() . '</error>');
+            $this->output->writeln('<comment>Reduce the number of sorting attributes that have enabled virtual replicas and try again.</comment>');
+            return CLI::RETURN_FAILURE;
+        } catch (BadRequestException $e) {
+            $this->output->writeln('<error>' . $e->getMessage() . '</error>');
+            if ($storeIds) {
+                $this->output->writeln('<comment>Your Algolia application may contain cris-crossed replicas. Try running "algolia:replicas:rebuild" for all stores to correct this.');
+            }
+            return CLI::RETURN_FAILURE;
+        }
+
+        return Cli::RETURN_SUCCESS;
     }
 
 }
